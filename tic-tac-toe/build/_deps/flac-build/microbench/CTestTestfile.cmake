9c1ed155c775ae32fe9c491a17bbca673b5d7a30
zhuyingb <yingbing.zhu@hotmail.com>
2025-03-13 22:00:41 -0700

    Untrack build directory and its contents

--- tic-tac-toe/build/_deps/flac-build/microbench/CTestTestfile.cmake
+++
@@ -1,6 +0,0 @@
-# CMake generated Testfile for 
-# Source directory: /Users/yingbingzhu/CLionProjects/cpp-mini-projects/tic-tac-toe/build/_deps/flac-src/microbench
-# Build directory: /Users/yingbingzhu/CLionProjects/cpp-mini-projects/tic-tac-toe/build/_deps/flac-build/microbench
-# 
-# This file includes the relevant testing commands required for 
-# testing this directory and lists subdirectories to be tested as well.
--- tic-tac-toe/build/_deps/flac-build/src/libFLAC/CTestTestfile.cmake
+++
@@ -1,6 +0,0 @@
-# CMake generated Testfile for 
-# Source directory: /Users/yingbingzhu/CLionProjects/cpp-mini-projects/tic-tac-toe/build/_deps/flac-src/src/libFLAC
-# Build directory: /Users/yingbingzhu/CLionProjects/cpp-mini-projects/tic-tac-toe/build/_deps/flac-build/src/libFLAC
-# 
-# This file includes the relevant testing commands required for 
-# testing this directory and lists subdirectories to be tested as well.
