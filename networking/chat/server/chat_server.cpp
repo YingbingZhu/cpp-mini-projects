636932a50f72f4fe94726013b85164fec766ec37
zhuyingb <yingbing.zhu@hotmail.com>
2025-05-02 21:46:23 -0700

    secure server

--- networking/chat/server/chat_server.cpp
+++ networking/chat/server/chat_server.cpp
@@ -15,15 +15,16 @@
 #include <openssl/ssl.h>
 #include <openssl/err.h>
 
-std::vector<int> clients;
+std::vector<SSL*> clients;
 std::mutex clients_mutex;
+SSL_CTX *ssl_ctx; // manage certificates and algorithm
 
-void handle_client(int client_socket) {
+void handle_client(SSL* ssl, int client_socket) {
   char buffer[1024];
 
   while (true) {
     memset(buffer, 0, sizeof(buffer));
-    ssize_t bytes_read = recv(client_socket, buffer, sizeof(buffer), 0);
+    ssize_t bytes_read = SSL_read(ssl, buffer, sizeof(buffer));
     if (bytes_read <= 0) {
       std::cerr << "Client disconnected or error\n";
       break;
@@ -32,9 +33,9 @@ void handle_client(int client_socket) {
     std::cout << "Received " << bytes_read << " bytes\n";
     // broadcasr the messgae to all clients
     clients_mutex.lock();
-    for (int client: clients) {
-      if (client != client_socket) {
-        send(client, buffer, bytes_read, 0);
+    for (SSL* other_ssl : clients) {
+      if (other_ssl != ssl) {
+        SSL_write(other_ssl, buffer, bytes_read);
       }
     }
     clients_mutex.unlock();
@@ -43,14 +44,45 @@ void handle_client(int client_socket) {
   // cleanup
   clients_mutex.lock();
   clients.erase(
-      std::remove(clients.begin(), clients.end(), client_socket),// shift elements that not match to front
+      std::remove(clients.begin(), clients.end(), ssl),
       clients.end()
   );
   clients_mutex.unlock();
-  close(client_socket);
+  SSL_shutdown(ssl);
+  SSL_free(ssl);
+}
+
+void print_current_dir() {
+  char buff[PATH_MAX];
+  if (getcwd(buff, sizeof(buff)) != NULL) {
+    std::cout << "Current working directory: " << buff << std::endl;
+  } else {
+    std::cerr << "Failed to get current working directory!" << std::endl;
+  }
 }
 
 int main() {
+  print_current_dir();
+  // Initialize OpenSSL
+  SSL_library_init();
+  SSL_load_error_strings();
+  OpenSSL_add_all_algorithms();
+
+  // Create SSL context
+  ssl_ctx = SSL_CTX_new(TLS_server_method());
+  if (!ssl_ctx) {
+    ERR_print_errors_fp(stderr);
+    return 1;
+  }
+
+  // Load certificate and private key
+  if (SSL_CTX_use_certificate_file(ssl_ctx, "../../../cert.pem", SSL_FILETYPE_PEM) <= 0 ||
+    SSL_CTX_use_PrivateKey_file(ssl_ctx, "../../../key.pem", SSL_FILETYPE_PEM) <= 0) {
+    ERR_print_errors_fp(stderr);
+    return 1;
+    }
+
+
   int sock = socket(AF_INET, SOCK_STREAM, 0);
   if (sock == -1) {
     std::cerr << "Failed to create socket\n";
@@ -79,19 +111,24 @@ int main() {
     sockaddr_in client_addr;
     socklen_t len = sizeof(client_addr);
     int conn = accept(sock, (sockaddr*)&client_addr, &len);
-    if (conn == -1) {
+    SSL* ssl = SSL_new(ssl_ctx);
+    SSL_set_fd(ssl, conn);
+    if (SSL_accept(ssl) <= 0) {
       std::cerr << "Failed to accept connection\n";
       continue;
     }
     std::cout << "New connection from " << inet_ntoa(client_addr.sin_addr) << "\n";
     // add to client list
     clients_mutex.lock();
-    clients.push_back(conn);
+    clients.push_back(ssl);
     clients_mutex.unlock();
 
     // create a thread for this client
-    std::thread(handle_client, conn).detach();
+    std::thread(handle_client, ssl, conn).detach();
   }
+
+  SSL_CTX_free(ssl_ctx);
+  EVP_cleanup();
 
   return 0;
 }
